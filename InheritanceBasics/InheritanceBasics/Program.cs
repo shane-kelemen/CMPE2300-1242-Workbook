b4fbd1f339bd22c7c6a0611170dc6eb735d86183
shane-kelemen <43184468+shane-kelemen@users.noreply.github.com>
2025-03-19 09:43:20 -0600

    Initial Commit - Not Commented
    Includes interaction with class library.  Experiment with moving things around to explore how the access specifications work.

---
+++ InheritanceBasics/InheritanceBasics/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using SomeLibrary;
+
+namespace InheritanceBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object thing = new object();
+            Shape myShape = new Shape(new Point(5,5), Color.Green);
+            Console.WriteLine(myShape);
+            myShape.Move();
+            Console.WriteLine(myShape);
+
+            thing = myShape;
+            Circle myCircle = new Circle(25.0, new Point(20, 20));
+            Console.WriteLine(myCircle);
+            myCircle.Move();
+            Console.WriteLine(myCircle);
+
+            myShape = myCircle;
+            //Square mySquare;
+
+            //if (myShape is Square)
+            //     mySquare = (Square)myShape;
+
+            if (myShape is Circle)
+            {
+                myCircle = myShape as Circle;
+                Console.WriteLine("Found a Circle");
+            }
+            Console.ReadKey();
+        }
+    }
+
+ 
+
+    
+
+    //class Rect : Shape
+    //{
+    //    double length;
+    //    double Width;
+
+    //    double CalcArea()
+    //    {
+    //        return length * Width;
+    //    }
+
+    //    void Move()
+    //    {
+
+    //    }
+    //}
+
+    //class Square : Rect
+    //{
+        
+    //}
+}
---
+++ InheritanceBasics/SomeLibrary/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SomeLibrary
+{
+    public class Shape
+    {
+        private protected Point _location;
+        private protected double _area;
+        private protected Color _colour;
+
+        public Shape(Point location, Color colour)
+        {
+            Console.WriteLine("Shape Constructor");
+            _location = location;
+            _colour = colour;
+        }
+
+        public void Move()
+        {
+            _location = new Point(_location.X + 100, _location.Y + 100);
+        }
+
+        public override string ToString()
+        {
+            return _location.ToString();
+        }
+    }
+
+    public class Circle : Shape, IComparable
+    {
+        double _radius;
+
+        public Circle(double radius, Point location) : base(location, Color.Red)
+        {
+            Console.WriteLine("Circle Constructor");
+            _radius = radius;
+
+            Random rng = new Random();
+            _colour = Color.FromArgb(rng.Next(256), rng.Next(256), rng.Next(256));
+        }
+
+
+        double CalcArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+
+        public new void Move()
+        {
+            base.Move();
+            _location = new Point(_location.X + 500, _location.Y + 500);
+        }
+
+        public int CompareTo(object obj)
+        {
+            return 1;
+        }
+    }
+}
