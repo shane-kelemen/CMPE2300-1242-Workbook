007311754851d8e2d94c1b8e19476d33f56d9cef
shane-kelemen <43184468+shane-kelemen@users.noreply.github.com>
2025-04-02 09:26:44 -0600

    Example from Class.  Commented

---
+++ NVIPattern/NVIPattern/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVIPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BaseClass> stuff = new List<BaseClass>();
+
+            for(int i = 0; i < 10; ++i)
+            {
+                stuff.Add(new Doubler());
+                stuff.Add(new Tripler());
+                stuff.Add(new Quadder());
+            }
+
+            foreach (BaseClass thing in stuff)
+                Console.Write(thing);
+            Console.WriteLine();
+
+            foreach (BaseClass thing in stuff)
+                thing.ChangeValue();
+
+            foreach (BaseClass thing in stuff)
+                Console.Write(thing);
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+
+
+    abstract class BaseClass
+    {
+        static Random rng = new Random();
+        protected int value;
+
+        public BaseClass()
+        {
+            value = rng.Next(100);
+        }
+           
+        // A regular public method through which the user must access the 
+        // desired polymorphic behaviour of the hierarchy.
+        public void ChangeValue()
+        {
+            VChangeValue();     // Engage the hidden polymorphic hierarchy behaviour
+            value *= -1;        // Add additional functionality that will apply
+                                // to all instances in the hierarchy, even in newly
+                                // derived classes.
+        }
+
+        // A polymorphic enabled method hidden from the user's direct access so
+        // they are forced to use the public base class method.  This provides us the
+        // ability to "gatekeep" access to the polymorphic behaviour and add on
+        // additional functionality in the base class method the user must use if we
+        // want to do so.
+        // 
+        // Making the method abstract also forces the user to implement the polymorhic 
+        // method for any further dervied classes.
+        protected abstract void VChangeValue();
+
+        public override string ToString()
+        {
+            return value + ", ";
+        }
+    }
+
+
+    class Doubler : BaseClass
+    {
+        // Override the polymorphic abstract method from the base class and add
+        // functionality in the spirit of the hierarchy.
+        protected override void VChangeValue()
+        {
+            value *= 2;
+        }
+    }
+
+    class Tripler : BaseClass
+    {
+        protected override void VChangeValue()
+        {
+            value *= 3;
+        }
+    }
+
+    class Quadder : BaseClass
+    {
+        protected override void VChangeValue()
+        {
+            value *= 4;
+        }
+    }
+}
---
+++ NVIPattern/NVIPattern/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NVIPattern")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NAIT")]
+[assembly: AssemblyProduct("NVIPattern")]
+[assembly: AssemblyCopyright("Copyright © NAIT 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("76eb61cf-27e8-4ecb-9d56-1d4dccbbe697")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
